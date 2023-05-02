2c4441936e61052dfb4ecec8cc76d656f8b70fb4
SergioTabares <sergio_andrestb@yahoo.es>
2023-05-02 09:07:51 -0500

    Commit inicial

---
+++ js/encriptar.js
@@ -0,0 +1,50 @@
+
+function encriptadoTexto(){
+    var ParrafoInicial = document.getElementById("text-ingreso").innerHTML;
+    var ParrafoResultado = document.getElementById("respueta");
+    ParrafoResultado.innerHTML = desencriptar(ParrafoInicial);
+}
+function desencriptarTexto(){
+
+}
+function copiarTextoEncriptado(){
+
+}
+function desencriptar(Texto){
+    
+
+}
+function desencriptar(){
+
+}
+
+
+button.onclick("boton-encripatar") = encriptadoTexto;
+
+
+/* 
+var texto = "Este es un texto con acentos: áéíóú";
+var regex = /[áéíóú]/gi; // Expresión regular para buscar acentos
+if (regex.test(texto)) {
+  console.log("El texto contiene acentos");
+} else {
+  console.log("El texto no contiene acentos");
+}
+
+var texto = "Este es un texto con PALABRAS en MAYÚSCULAS";
+var regex = /[A-Z]{2,}/g; // Expresión regular para buscar palabras en mayúsculas
+if (regex.test(texto)) {
+  console.log("El texto contiene palabras en mayúsculas");
+} else {
+  console.log("El texto no contiene palabras en mayúsculas");
+}
+
+var texto = "Este es un texto con caracteres especiales: !@#$%^&*()_+-={}[]|\\;:'\",.<>/?";
+var regex = /[!@#$%^&*()_+\-={}[\]\\|;:'",.<>/?]/g; // Expresión regular para buscar caracteres especiales
+if (regex.test(texto)) {
+  console.log("El texto contiene caracteres especiales");
+} else {
+  console.log("El texto no contiene caracteres especiales");
+}
+
+*/
